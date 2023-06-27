33cc07a0fe92aa90343383c78224387a97139d55
Kévin PEREZ <kevin.perez@ca-gip.fr>
2023-06-27 15:21:04 +0200

    🗑️ Clean code not used

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -14,16 +14,3 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-
 bash "${CODEGEN_PKG}"/generate-groups.sh all \
   github.com/ca-gip/kotary/pkg/generated github.com/ca-gip/kotary/pkg/apis \
   cagip:v1  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
-
-# # Dirty fix fake clientset
-# # GroupName needs to be updated manually to ca-gip.cagip.com
-# # Issue open https://github.com/kubernetes/code-generator/issues/98
-# BASE_PATH=$(pwd "${SCRIPT_ROOT}")
-# FILE_TO_FIX="${BASE_PATH}/pkg/generated/clientset/versioned/typed/ca-gip/v1/fake/fake_resourcequotaclaim.go"
-# SED_EXP='s/cagip.github.com/ca-gip.github.com/g'
-
-# echo "Fixing group name in fake clienset"
-
-# if [ "${OSTYPE//[0-9.]/}" == "darwin" ]; then sed -i '' ${SED_EXP} ${FILE_TO_FIX}
-# else sed -i ${SED_EXP} ${FILE_TO_FIX}
-# fi
