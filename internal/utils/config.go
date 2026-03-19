597ad1cb726fdaaec72d893593b1946a9232f76d
slim-d <slimdah3@gmail.com>
2026-03-19 14:06:41 +0100

    feat: Add Prometheus metrics for Kotary configuration parameters (#19)
    * feat: add Prometheus metrics for Kotary configuration limits
    
    * clean: minor cleanup
    
    ---------
    
    Co-authored-by: Slimane DAHMANI <slimane.dahmani-prestataire@ca-gip.fr>

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -74,7 +74,7 @@ func (c *ConfigurationManager) generateDefaultSettings() *Config {
 	klog.V(6).Info("Default setting will not select any Namespaces to provision default ResourceQuotaClaim")
 	klog.V(6).Info("Default will not apply over commitment to Nodes available resources")
 
-	return &Config{
+	defaultConfig := &Config{
 		DefaultClaimSpec:         *claimSpecByDefault,
 		RatioMaxAllocationMemory: defaultMaxAllocationMemory,
 		RatioMaxAllocationCPU:    defaultMaxAllocationCPU,
@@ -83,6 +83,10 @@ func (c *ConfigurationManager) generateDefaultSettings() *Config {
 		MaxJobsLimitNS:           defaultMaxJobsLimitNS,
 		MaxJobsLimitCluster:      defaultMaxJobsLimitCluster,
 	}
+
+	setKotaryMetrics(defaultConfig)
+
+	return defaultConfig
 
 }
 
@@ -93,7 +97,7 @@ func (c *ConfigurationManager) Load() {
 
 	if err != nil {
 		klog.Infof("Could not load namespace via %s ", nsSecretPath)
-		c.Conf = *c.generateDefaultSettings()
+		c.Conf = *c.generateDefaultSettings()	
 		return
 	}
 
@@ -194,7 +198,20 @@ func parseConfigMap(configMap *v1.ConfigMap) (parsed *Config, err error) {
 	}
 
 	klog.Infof("Loaded config map : %+v\n", parsed)
-
+	setKotaryMetrics(parsed)
 	return
 
 }
+
+func setKotaryMetrics(kotaryConfig *Config) {
+
+	MaxJobsLimitNSGauge.Set(float64(kotaryConfig.MaxJobsLimitNS))
+	MaxJobsLimitClusterGauge.Set(float64(kotaryConfig.MaxJobsLimitCluster))
+	RatioMaxAllocationCPUGauge.Set(float64(kotaryConfig.RatioMaxAllocationCPU))
+	RatioMaxAllocationMemoryGauge.Set(float64(kotaryConfig.RatioMaxAllocationMemory))
+	RatioOverCommitCPUGauge.Set(float64(kotaryConfig.RatioOverCommitCPU))
+	RatioOverCommitMemoryGauge.Set(float64(kotaryConfig.RatioOverCommitMemory))
+
+	klog.Infof("Kotary metrics updated from configuration")
+}
+
--- internal/utils/prometheus.go
+++ internal/utils/prometheus.go
@@ -14,3 +14,33 @@ var DefaultClaimCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 	Name: "rq_operator_default_claims",
 	Help: "Number of default claims created",
 }, []string{"status"})
+
+var MaxJobsLimitNSGauge = promauto.NewGauge(prometheus.GaugeOpts{
+	Name: "kotary_max_jobs_batch_limit_ns",
+	Help: "Maximum number of jobs allowed per namespace",
+   })
+   
+var MaxJobsLimitClusterGauge = promauto.NewGauge(prometheus.GaugeOpts{
+	Name: "kotary_max_jobs_batch_limit_cluster",
+	Help: "Maximum total number of jobs allowed across the cluster",
+})
+
+var RatioMaxAllocationCPUGauge = promauto.NewGauge(prometheus.GaugeOpts{
+    Name: "kotary_ratio_max_allocation_cpu",
+    Help: "Maximum CPU allocation ratio allowed per namespace compared to total cluster CPU",
+})
+
+var RatioMaxAllocationMemoryGauge = promauto.NewGauge(prometheus.GaugeOpts{
+    Name: "kotary_ratio_max_allocation_memory", 
+    Help: "Maximum memory allocation ratio allowed per namespace compared to total cluster memory",
+})
+
+var RatioOverCommitCPUGauge = promauto.NewGauge(prometheus.GaugeOpts{
+    Name: "kotary_ratio_over_commit_cpu",
+    Help: "CPU over-commit ratio applied to node available resources (percentage)",
+})
+
+var RatioOverCommitMemoryGauge = promauto.NewGauge(prometheus.GaugeOpts{
+    Name: "kotary_ratio_over_commit_memory",
+    Help: "Memory over-commit ratio applied to node available resources (percentage)", 
+})
